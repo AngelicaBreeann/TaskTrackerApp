ef7298ad2924b49caa65e5822045a806e3f8b871
Angelica Olmedo <olmedoangelica23@gmail.com>
2022-12-31 02:21:29 -0800

    Added- bottom navigation 3 fragments

--- TaskTracker/app/src/main/java/com/example/tasktracker/MainActivity.kt
+++ TaskTracker/app/src/main/java/com/example/tasktracker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.tasktracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val homefrag = HomeFragment()
+    private val datafrag = DataFragment()
+    private val settingsfrag = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(homefrag)
+
+        findViewById<BottomNavigationView>(R.id.nav_view).setOnNavigationItemSelectedListener{
+                item-> when(item.itemId){
+            R.id.data -> replaceFragment(datafrag)
+            R.id.settings -> replaceFragment(settingsfrag)
+        }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container, fragment)
+            transaction.commit()
+        }
     }
 }
